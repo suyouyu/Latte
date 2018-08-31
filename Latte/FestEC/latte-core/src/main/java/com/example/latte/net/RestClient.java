9748e31dfbd73002f490be8b98a89943260c5d62
suyouyu <694079250@qq.com>
2018-08-31 18:02:54 +0800

    latte 大致完成 5 7

--- Latte/FestEC/latte-core/src/main/java/com/example/latte/net/RestClient.java
+++ Latte/FestEC/latte-core/src/main/java/com/example/latte/net/RestClient.java
@@ -28,7 +28,7 @@ public class RestClient {
 
     //声明需要的变量
     private final String URL;
-    private static final WeakHashMap<String,Object> PARAMS = RestCreator.getParams();
+    private static final WeakHashMap<String, Object> PARAMS = RestCreator.getParams();
     private final IRequest REQUEST;
     private final ISuccess SUCCESS;
     private final IFailure FAILURE;
@@ -57,43 +57,49 @@ public class RestClient {
         this.LOADER_STYLE = loaderStyle;
     }
 
-    public static RestClientBuilder builder(){
+    public static RestClientBuilder builder() {
         return new RestClientBuilder();
     }
 
-    private void request(HttpMethod method){
+    private void request(HttpMethod method) {
         final RestService service = RestCreator.getRestService();
         retrofit2.Call<String> call = null;
-        if (REQUEST != null){
+        if (REQUEST != null) {
             REQUEST.onRequestStart();
         }
-        if (LOADER_STYLE != null){
-            LatteLoader.showLoading(CONTEXT,LOADER_STYLE);
+        if (LOADER_STYLE != null) {
+            LatteLoader.showLoading(CONTEXT, LOADER_STYLE);
         }
-        switch (method){
+        switch (method) {
             case GET:
-                call = service.get(URL,PARAMS);
+                call = service.get(URL, PARAMS);
                 break;
             case POST:
-                call = service.post(URL,PARAMS);
+                call = service.post(URL, PARAMS);
+                break;
+            case POST_RAW:
+                call = service.postRaw(URL, BODY);
                 break;
             case PUT:
-                call = service.put(URL,PARAMS);
+                call = service.put(URL, PARAMS);
+                break;
+            case PUT_RAW:
+                call = service.putRaw(URL, BODY);
                 break;
             case DELETE:
-                call = service.delete(URL,PARAMS);
+                call = service.delete(URL, PARAMS);
                 break;
             default:
                 break;
         }
 
-        if (call != null){
+        if (call != null) {
             //这个方法会在后台线程执行，不会影响主线程
             call.enqueue(getRequestCallback());
         }
     }
 
-    private Callback<String> getRequestCallback(){
+    private Callback<String> getRequestCallback() {
         return new RequestCallbacks(
                 REQUEST,
                 SUCCESS,
@@ -102,19 +108,33 @@ public class RestClient {
                 LOADER_STYLE);
     }
 
-    public final void get(){
+    public final void get() {
         request(HttpMethod.GET);
     }
 
-    public final void post(){
-        request(HttpMethod.POST);
+    public final void post() {
+        if (BODY == null) {
+            request(HttpMethod.POST);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null");
+            }
+            request(HttpMethod.POST_RAW);
+        }
     }
 
-    public final void put(){
-        request(HttpMethod.PUT);
+    public final void put() {
+        if (BODY == null) {
+            request(HttpMethod.PUT);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null");
+            }
+            request(HttpMethod.POST_RAW);
+        };
     }
 
-    public final void delete(){
+    public final void delete() {
         request(HttpMethod.DELETE);
     }
 }
